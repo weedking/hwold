ce1710adc3fde41efc2def5008740c163cf9da4a
weedking <260815998@qq.com>
2017-03-25 07:47:14 +0800

    Initial commit

---
+++ src/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package demo;
+
+/**
+ * Created by lihaiming on 2017/3/25.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("hello world11!");
+    }
+}
